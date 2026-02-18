fcae49a0d0ee759ca8c8f4dd9f890796d104c23f
onikageyoshi <davidonyekachi29@gmail.com>
2026-02-18 14:45:55 +0100

    deployment dependencies added last one again

--- core/settings.py
+++ core/settings.py
@@ -11,7 +11,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", "unsafe-secret-key-for-dev")
 DEBUG = os.getenv("DJANGO_DEBUG", "True") == "True"
-ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS", "").split(",")
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "booker-6jxi.onrender.com"] 
 
 
 INSTALLED_APPS = [
